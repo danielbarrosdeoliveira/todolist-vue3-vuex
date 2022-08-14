6856ce0da401fb3553df3255314fb090b76b4b13
Daniel Oliveira <d.dani_oli@hotmaiil.com>
2022-08-14 03:03:25 -0300

    :recycle: refactor component to Composition API

--- src/components/TodoItems.vue
+++ src/components/TodoItems.vue
@@ -1,20 +1,23 @@
 <template>
   <div class="space-y-2">
-    <TodoItem v-for="todo in todos" :key="todo.id" :todo="todo" />
+    <TodoItem v-for="todo in allTodos" :key="todo.id" :todo="todo" />
   </div>
 </template>
 
 <script>
+import { ref } from 'vue';
 import TodoItem from './TodoItem.vue';
 
 export default {
   name: 'TodoItems',
+  props: ['todos'],
   components: { TodoItem },
-  props: {
-    todos: {
-      type: Object,
-      default: () => {}
-    }
+  setup(props) {
+    const allTodos = ref(props.todos);
+
+    return {
+      allTodos
+    };
   }
 };
 </script>
