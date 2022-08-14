5a73825cf1a6b698d3c3bfb62f9d737a1523bc48
Daniel Oliveira <d.dani_oli@hotmaiil.com>
2022-08-14 00:35:46 -0300

    :sparkles: defined store loading and todos, using axios on actions

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,45 @@
 import { createStore } from 'vuex';
+import { http } from '@/services/http';
 
 export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
+  state: {
+    todos: [],
+    loading: false
+  },
+  getters: {
+    $getTodos(state) {
+      return state.todos;
+    },
+    $getLoading(state) {
+      return state.loading;
+    }
+  },
+  mutations: {
+    SET_TODOS(state, payload) {
+      state.todos = payload;
+    },
+    SET_LOADING(state, payload) {
+      state.loading = payload;
+    }
+  },
+  actions: {
+    async getTodos({ commit, dispatch }) {
+      dispatch('updateLoading', true);
+      try {
+        const { data } = await http.get('todos');
+
+        commit('SET_TODOS', data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setTimeout(() => {
+          dispatch('updateLoading', false);
+        }, 3000);
+      }
+    },
+    updateLoading({ commit }, payload) {
+      commit('SET_LOADING', payload);
+    }
+  },
   modules: {}
 });
